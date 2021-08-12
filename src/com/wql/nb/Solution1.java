c403b3474bc50b44e4f7816ec5b39d0af90a129e
wql <1242255968@qq.com>
2021-08-13 00:03:59 +0800

    0:03

---
+++ src/com/wql/nb/Solution1.java
@@ -0,0 +1,13 @@
+package com.wql.nb;
+
+/**
+ * @Description: TODO
+ * @Author: santiago
+ * @Date: 2021-08-12 23:51
+ */
+public class Solution1 {
+    public static void main(String[] args) {
+        System.out.println("20210812351");
+        System.out.println("202108130003");
+    }
+}
