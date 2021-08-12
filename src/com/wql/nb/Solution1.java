59aad3bb9882d6c6f891d3e69615f9fa92291dbc
wql <1242255968@qq.com>
2021-08-13 00:33:59 +0800

    0:33 dcn 提交

---
+++ src/com/wql/nb/Solution1.java
@@ -0,0 +1,12 @@
+package com.wql.nb;
+
+/**
+ * @Description: TODO
+ * @Author: santiago
+ * @Date: 2021-08-13 00:20
+ */
+public class Solution1 {
+    public static void main(String[] args) {
+        System.out.println("0:21 dcn 提交");
+    }
+}
