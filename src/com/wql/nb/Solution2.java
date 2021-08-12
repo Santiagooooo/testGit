809f02e5718f0498a86cd68ed0ee84e1fc4ecdde
wql <1242255968@qq.com>
2021-08-13 00:06:36 +0800

    0:06

---
+++ src/com/wql/nb/Solution2.java
@@ -0,0 +1,12 @@
+package com.wql.nb;
+
+/**
+ * @Description: TODO
+ * @Author: santiago
+ * @Date: 2021-08-13 00:05
+ */
+public class Solution2 {
+    public static void main(String[] args) {
+        System.out.println("新建master分支");
+    }
+}
